92bbfd8d9c832649edf1e33493a0e8e22b40940f
ArtemKozak <a.s.kozak@ukr.net>
2023-07-06 19:32:09 +0300

    hw2_task2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import Flights from "./components/flights/Flights";
 
 function App() {
   return (
       <div>
+          <Flights/>
+
       </div>
   );
 
 }
 
+// є API от SpaceX
+// https://api.spacexdata.com/v3/launches/
+//     потрібно вивести всі запуски кораблів окрім 2020 року
+// репрезентувати тільки окремі поля (зазначені в скрнішоті в папці)
 export default App;
